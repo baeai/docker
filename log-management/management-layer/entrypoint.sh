cebeb09691ad9d49ef9a7b7943aa9735ddd09aa7
root <root@docker.silversky.com>
2015-08-05 18:27:56 -0400

    update management-layer

--- log-management/management-layer/entrypoint.sh
+++ log-management/management-layer/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CIDR=$(ip route|awk 'NR==2'|awk -F' ' '{print $1}')
+
 if [[ ! -e /postgresql.installed ]];
 then
 
@@ -12,13 +14,15 @@ then
     
 ##Initializing DB
 su postgres -c "initdb"
-echo "host all all 172.17.0.0/16 trust" >>/var/lib/postgresql/data/pg_hba.conf
+echo "host all all $CIDR trust" >>/var/lib/postgresql/data/pg_hba.conf
 echo "listen_addresses='*'" >>/var/lib/postgresql/data/postgresql.conf
 su postgres -c "postgres"
 su postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password '$POSTGRES_PASSWORD';\"" \
 && touch /postgresql.installed
+psql --file /usr/share/pgsql/contrib/adminpack.sql
 else
 
 su postgres "$@"
 
 fi
+
